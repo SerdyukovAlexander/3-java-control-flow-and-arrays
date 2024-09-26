bef745c6731d8aad73ead7ad964c3d212bf5e8c6
Serdyukov Alexandr <serdyukov2018@bk.ru>
2024-09-26 10:49:11 +0500

    Задание 5

--- task05/src/com.example.task05/Task05Main.java
+++ task05/src/com.example.task05/Task05Main.java
@@ -1,16 +1,26 @@
 package com.example.task05;
 
-public class Task05Main {
-    public static void main(String[] args) {
-        //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
+public class Task05Main
+{
+    public static void main(String[] args)
+    {
         System.out.println(getMin(5,4,3));
-         */
     }
 
-    static int getMin(int a, int b, int c) {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
+    static int getMin(int a, int b, int c)
+    {
+        if(a<=b&&a<=c)
+        {
+            return a;
+        }
+        else if(b<=a&&b<=c)
+        {
+            return b;
+        }
+        else if(c<=a&&c<=b)
+        {
+            return c;
+        }
         return 0;
     }
 }
