73374e04d0358db4aba8081c1b19505588998911
Serdyukov Alexandr <serdyukov2018@bk.ru>
2024-09-26 11:59:23 +0500

    Задание 12

--- task12/src/com.example.task12/Task12Main.java
+++ task12/src/com.example.task12/Task12Main.java
@@ -1,18 +1,43 @@
 package com.example.task12;
 
-public class Task12Main {
-    public static void main(String[] args) {
-        //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
+import java.util.Objects;
+
+public class Task12Main
+{
+    public static void main(String[] args)
+    {
         int[] arr = {9, 11, 7, 8};
         selectionSort(arr);
         System.out.println(java.util.Arrays.toString(arr));
-         */
     }
 
-    static void selectionSort(int[] arr) {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
+    static void selectionSort(int[] arr)
+    {
+        if(!Objects.isNull(arr) && arr.length>0)
+        {
+            int n = 0;
+
+            while (n < arr.length)
+            {
+                int minnum = Integer.MAX_VALUE;
+                int maxI = 0;
+                int buf;
+
+                for (int i = n; i < arr.length; i++)
+                {
+                    if (minnum >= arr[i])
+                    {
+                        minnum = arr[i];
+                        maxI = i;
+                    }
+                }
+
+                buf = arr[n];
+                arr[n] = minnum;
+                arr[maxI] = buf;
+                n += 1;
+            }
+        }
     }
 
 }
