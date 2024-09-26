1d20d02519d8673f76ab3f9ca5f4b434d3dc582a
Serdyukov Alexandr <serdyukov2018@bk.ru>
2024-09-26 10:54:50 +0500

    Задание 6

--- task06/src/com.example.task06/Task06Main.java
+++ task06/src/com.example.task06/Task06Main.java
@@ -1,17 +1,24 @@
 package com.example.task06;
 
-public class Task06Main {
-    public static void main(String[] args) {
-        //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
+import java.util.Arrays;
+
+public class Task06Main
+{
+    public static void main(String[] args)
+    {
         System.out.println(getMax(1, 2, 3, 4));
-         */
     }
 
-    static int getMax(int a, int b, int c, int d) {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
-        return 0;
+    static int getMax(int a, int b, int c, int d)
+    {
+        int[] result = new int[4];
+        result[0]=a;
+        result[1]=b;
+        result[2]=c;
+        result[3]=d;
+        Arrays.sort(result);
+
+        return result[3];
     }
 
 }
