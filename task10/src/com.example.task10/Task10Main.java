0d4a4d1871e876e7df276ee06ea767eed2732ab7
Serdyukov Alexandr <serdyukov2018@bk.ru>
2024-09-26 11:10:52 +0500

    Задание 10

--- task10/src/com.example.task10/Task10Main.java
+++ task10/src/com.example.task10/Task10Main.java
@@ -1,18 +1,27 @@
 package com.example.task10;
 
 public class Task10Main {
-    public static void main(String[] args) {
-        //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
-        int[] arr = {7, 5, 9};
+    public static void main(String[] args)
+    {
+        int[] arr = {7, 8, 9, 7, 7, 7};
         System.out.println(numMin(arr));
-         */
     }
 
-    static int numMin(int[] arr) {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
-        return 0;
+    static int numMin(int[] arr)
+    {
+        int minnum = 10*10;
+        int maxi = 0;
+
+        for(int i = 0; i < arr.length; i++)
+        {
+            if (minnum>=arr[i])
+            {
+                minnum=arr[i];
+                maxi=i;
+            }
+        }
+
+        return maxi;
     }
 
 }
