231761d48b5cde4c48aee65074911fc727072067
Serdyukov Alexandr <serdyukov2018@bk.ru>
2024-09-26 11:03:15 +0500

    Задание 8

--- task08/src/com.example.task08/Task08Main.java
+++ task08/src/com.example.task08/Task08Main.java
@@ -1,20 +1,31 @@
 package com.example.task08;
 
-public class Task08Main {
-    public static void main(String[] args) {
-        //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
+public class Task08Main
+{
+    public static void main(String[] args)
+    {
         int[] arr = new int[2];
         arr[0] = 2;
         arr[1] = 3;
         System.out.println(mult(arr));
-         */
+
     }
 
-    static long mult(int[] arr) {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
-        return 1;
+    static long mult(int[] arr)
+    {
+        if(arr.length>0)
+        {
+            long result = 1;
+
+            for (int i = 0; i < arr.length; i++)
+            {
+                result *= arr[i];
+            }
+
+            return result;
+        }
+
+        return 0;
     }
 
 }
